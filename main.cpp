49cb33ba6a7b9e6bf6b8f1b3b9b902a6cea52052
ronyjah <ronyjah@gmail.com>
2016-09-21 20:03:15 -0300

    Add files via upload
    implementado: bit stuffy e flag.

--- main.cpp
+++ main.cpp
@@ -1,24 +1,44 @@
 #include "Framing.h"
+#include <string.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <fstream>
+    using std::ofstream;
+#include <iostream>
+#include <vector>
+    using std::cout;
+    using std::endl;
+
+
 int main(){
-	//char * frame;
+	ofstream stream;
+	stream.open("/home/ronaldo/Documentos/PTC/workspace/protocol/myurl.txt");
 	const char * path = "/dev/ttyUSB0";
-	char flag = 0x7e;
-    char* data = (char*)malloc(10);
-    data[0] = flag;
-    data[1] = 'O';
-    data[2] = 'l';
+   char * data = (char*)malloc(19);
+    data[0] = 'O';
+    data[1] = 'l';
+    data[2] = 'a';
     data[3] = 'a';
-    data[4] = ' ';
-    data[5] = 'M';
-    data[6] = 'U';
-    data[7] = 'N';
-    data[8] = 'D';
-    data[9] = 'O';
+    data[4] = 'M';
+    data[5] = 0x7e;
+    data[6] = 'N';
+    data[7] = 'D';
+    data[8] = 'O';
+
+    	//Serial serial(path,9600);
+   	 Framing testa(8,1024);
+   	int idx = strlen(data);
+	testa.mountFrame(data,idx);
+	stream.write((char*)data,15);
+	stream.close();
 
 
-    Serial serial(path,9600);
-	Framing testa(serial,8,1024);
-    testa.send(data,10);
-    testa.receive(data);
+
+
+  //  testa.send(data1,11);
+   //: testa.receive(data1);
+
 return 0;
 }
